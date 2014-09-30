5e295f02a3aa42638a9ac433e481298b5f509ba6
Renato Marroquin <renatoj.marroquin@gmail.com>
2014-09-30 17:51:35 +0200

    Fixing VertexUtils

--- src/org/apache/giraph/gora/utils/VertexUtils.java
+++ src/org/apache/giraph/gora/utils/VertexUtils.java
@@ -1,5 +1,19 @@
 /**
+ *Licensed to the Apache Software Foundation (ASF) under one
+ *or more contributor license agreements.  See the NOTICE file
+ *distributed with this work for additional information
+ *regarding copyright ownership.  The ASF licenses this file
+ *to you under the Apache License, Version 2.0 (the"
+ *License"); you may not use this file except in compliance
+ *with the License.  You may obtain a copy of the License at
+ *
+  * http://www.apache.org/licenses/LICENSE-2.0
  * 
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
  */
 package org.apache.giraph.gora.utils;
 
@@ -34,43 +48,55 @@ public class VertexUtils {
     /* Vertex 0 */
     Utf8 id = new Utf8("0");
     GVertex vrtx = new GVertex();
+    GVertex.newBuilder().setVertexValue(0).build();
     vrtx.setVertexId(id);
-    vrtx.putToEdges(new Utf8("1"), new Utf8("1"));
-    vrtx.putToEdges(new Utf8("3"), new Utf8("3"));
+     
+    Map<CharSequence, CharSequence> edges = new HashMap<CharSequence, CharSequence>();
+    edges.put(new Utf8("1"), new Utf8("1"));
+    edges.put(new Utf8("3"), new Utf8("3"));
+    vrtx.setEdges(edges);
     genGraph.put(id.toString(), vrtx);
 
     /* Vertex 1 */
     vrtx = new GVertex();
     id = new Utf8("1");
     vrtx.setVertexId(id);
-    vrtx.putToEdges(new Utf8("0"), new Utf8("1"));
-    vrtx.putToEdges(new Utf8("2"), new Utf8("2"));
-    vrtx.putToEdges(new Utf8("3"), new Utf8("1"));
+    edges = new HashMap<CharSequence, CharSequence>();
+    edges.put(new Utf8("0"), new Utf8("1"));
+    edges.put(new Utf8("2"), new Utf8("2"));
+    edges.put(new Utf8("3"), new Utf8("1"));
+    vrtx.setEdges(edges);
     genGraph.put(id.toString(), vrtx);
 
     /* Vertex 2 */
     vrtx = new GVertex();
     id = new Utf8("2");
     vrtx.setVertexId(id);
-    vrtx.putToEdges(new Utf8("1"), new Utf8("2"));
-    vrtx.putToEdges(new Utf8("4"), new Utf8("4"));
+    edges = new HashMap<CharSequence, CharSequence>();
+    edges.put(new Utf8("1"), new Utf8("2"));
+    edges.put(new Utf8("4"), new Utf8("4"));
+    vrtx.setEdges(edges);
     genGraph.put(id.toString(), vrtx);
 
     /* Vertex 3 */
     vrtx = new GVertex();
     id = new Utf8("3");
     vrtx.setVertexId(id);
-    vrtx.putToEdges(new Utf8("0"), new Utf8("3"));
-    vrtx.putToEdges(new Utf8("1"), new Utf8("1"));
-    vrtx.putToEdges(new Utf8("4"), new Utf8("4"));
+    edges = new HashMap<CharSequence, CharSequence>();
+    edges.put(new Utf8("0"), new Utf8("3"));
+    edges.put(new Utf8("1"), new Utf8("1"));
+    edges.put(new Utf8("4"), new Utf8("4"));
+    vrtx.setEdges(edges);
     genGraph.put(id.toString(), vrtx);
 
     /* Vertex 4 */
     vrtx = new GVertex();
     id = new Utf8("4");
     vrtx.setVertexId(id);
-    vrtx.putToEdges(new Utf8("3"), new Utf8("4"));
-    vrtx.putToEdges(new Utf8("2"), new Utf8("4"));
+    edges = new HashMap<CharSequence, CharSequence>();
+    edges.put(new Utf8("3"), new Utf8("4"));
+    edges.put(new Utf8("2"), new Utf8("4"));
+    vrtx.setEdges(edges);
     genGraph.put(id.toString(), vrtx);
     
     return genGraph;
@@ -100,8 +126,8 @@ public class VertexUtils {
     int numEdges = maxEdges>0?randomGenerator.nextInt(maxEdges):0;
     for (int iCnt = 0; iCnt < numEdges; iCnt ++){
       // select a specific vertex
-      Edge tmpEdg = getValidEdge(vrtx,pGraph);
-      vrtx.putToEdges(tmpEdg.getVertexId(), new Utf8(String.valueOf(tmpEdg.getEdgeValue())));
+      GEdge tmpEdg = getValidEdge(vrtx,pGraph);
+      vrtx.getEdges().put(tmpEdg.getVertexInId(), String.valueOf(tmpEdg.getEdgeWeight()));
     }
     /**/System.out.println(vrtx.toString());
     return vrtx;
@@ -113,16 +139,16 @@ public class VertexUtils {
    * @param pGraph
    * @return
    */
-  private static Edge getValidEdge(GVertex pVertex, Map<String, GVertex> pGraph){
-    Edge edg = new Edge();
+  private static GEdge getValidEdge(GVertex pVertex, Map<String, GVertex> pGraph){
+    GEdge edg = new GEdge();
     Random randomGenerator = new Random();
     GVertex vrtxEnd = null;
     do{
       vrtxEnd = ((GVertex)pGraph.values().toArray()[(randomGenerator.nextInt(pGraph.size()-1))]);
     }while (!validateEdge(pVertex, vrtxEnd));
-    Utf8 sinkVrtx = vrtxEnd.getVertexId();
-    edg.setVertexId(new Utf8(sinkVrtx.toString()));
-    edg.setEdgeValue(randomGenerator.nextFloat());
+    Utf8 sinkVrtx = new Utf8(vrtxEnd.getVertexId().toString());
+    edg.setVertexInId(new Utf8(sinkVrtx.toString()));
+    edg.setEdgeWeight(randomGenerator.nextFloat());
     return edg;
   }
 
@@ -133,9 +159,9 @@ public class VertexUtils {
    * @return
    */
   private static boolean validateEdge(GVertex pVertexBegin, GVertex pVertexEnd){
-    Iterator<Utf8> it = pVertexBegin.getEdges().keySet().iterator();
+    Iterator<CharSequence> it = pVertexBegin.getEdges().keySet().iterator();
     while (it.hasNext()){
-      Utf8 tmpEdg = it.next();
+      Utf8 tmpEdg = new Utf8(it.next().toString());
       if (tmpEdg.equals(pVertexEnd.getVertexId()))
         return false;
     }
